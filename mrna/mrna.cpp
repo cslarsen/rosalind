3cda2c48c48f372af918fee0d836658fdd031e6a
Christian Stigen Larsen <csl@sublevel3.org>
2012-11-14 03:20:53 +0100

    Cleanup

--- mrna/mrna.cpp
+++ mrna/mrna.cpp
@@ -4,17 +4,15 @@
 using namespace std;
 
 /*
- * (RNA codon) amino acid frequency, e.g. GCU, GCC, GCA and GCG
- * translate to A = 4.
+ * Amino acid frequency in RNA codon table.
+ *
+ * E.g., GCU, GCC, GCA and GCG gives A = 4.
+ *
+ * Range is from 'A' to 'Z'
  */
-size_t codonfreq[] = {
-  /* A */ 4,  /* B */ 0,  /* C */ 2,  /* D */ 2,
-  /* E */ 2,  /* F */ 2,  /* G */ 4,  /* H */ 2,
-  /* I */ 3,  /* J */ 0,  /* K */ 2,  /* L */ 6,
-  /* M */ 1,  /* N */ 2,  /* O */ 0,  /* P */ 4,
-  /* Q */ 2,  /* R */ 6,  /* S */ 6,  /* T */ 4,
-  /* U */ 0,  /* V */ 4,  /* W */ 1,  /* X */ 0,
-  /* Y */ 2,  /* Z */ 0,  /* STOP */ 3
+size_t codon_freq[] = {
+  4, 0, 2, 2, 2, 2, 4, 2, 3, 0, 2, 6, 1,
+  2, 0, 4, 2, 6, 6, 4, 0, 4, 1, 0, 2, 0
 };
 
 int main()
@@ -22,10 +20,11 @@ int main()
   string s;
   cin >> s;
 
-  size_t n = 3; // stop codons
+  // Initialize with the three stop codons
+  size_t n = 3;
 
-  for ( auto ch : s ) {
-    n *= codonfreq[ch - 'A'];
+  for ( auto c : s ) {
+    n *= codon_freq[c - 'A'];
     n %= 1000000;
   }
 
